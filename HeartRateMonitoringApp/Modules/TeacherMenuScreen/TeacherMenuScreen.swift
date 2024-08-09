cd4d9909826aa11c5c5d1d8541354b4dbd0901fa
João Rouxinol <m54076@alunos.uevora.pt>
2024-08-09 22:43:37 +0100

    Added Logout

--- HeartRateMonitoringApp/Modules/TeacherMenuScreen/TeacherMenuScreen.swift
+++ HeartRateMonitoringApp/Modules/TeacherMenuScreen/TeacherMenuScreen.swift
@@ -8,9 +8,12 @@
 import SwiftUI
 
 struct TeacherMenuScreen: View {
+    @Environment(\.presentationMode) var presentationMode
     @Binding var path: NavigationPath
     @State var teacher: Teacher
-    
+    @State private var showingAlert = false
+    @State private var isLogoutLoading = false
+
     var body: some View {
         ZStack {
             VStack {
@@ -30,37 +33,69 @@ struct TeacherMenuScreen: View {
                 HStack (spacing: 5) {
                     MainMenuSection(sectionColor: .red,
                                     sectionIcon: MainMenuIcons.myClassesIcon,
-                                    sectionTitle: localized(MainMenuStrings.classesSectionTitle),
-                                    sectionDescription: localized(MainMenuStrings.classesSectionDescription),
+                                    sectionTitle: "Agendar",
+                                    sectionDescription: "Agendar uma nova sessão para uma data futura",
                                     isUnavailable: false,
                                     sectionAction: {
                     })
                     MainMenuSection(sectionColor: .green,
                                     sectionIcon: MainMenuIcons.calendarIcon,
-                                    sectionTitle: localized(MainMenuStrings.calendarSectionTitle),
-                                    sectionDescription: localized(MainMenuStrings.calendarSectionDescription),
+                                    sectionTitle: "Agendamentos",
+                                    sectionDescription: "Visualizar sessões futuras e os seus participantes",
                                     isUnavailable: false,
                                     sectionAction: {
                         })
                 }
                 HStack (spacing: 5) {
                     MainMenuSection(sectionColor: .yellow,
-                                    sectionIcon: MainMenuIcons.myClassesIcon,
-                                    sectionTitle: localized(MainMenuStrings.classesSectionTitle),
-                                    sectionDescription: localized(MainMenuStrings.classesSectionDescription),
+                                    sectionIcon: "clock.arrow.circlepath",
+                                    sectionTitle: "Histórico",
+                                    sectionDescription: "Visualizar sessões e participantes das sessões passadas",
                                     isUnavailable: false,
                                     sectionAction: {
                     })
                     MainMenuSection(sectionColor: .blue,
-                                    sectionIcon: MainMenuIcons.myClassesIcon,
-                                    sectionTitle: localized(MainMenuStrings.classesSectionTitle),
-                                    sectionDescription: localized(MainMenuStrings.classesSectionDescription),
+                                    sectionIcon: MainMenuIcons.logoutIcon,
+                                    sectionTitle: "Logout",
+                                    sectionDescription: "Sair para o menu principal",
                                     isUnavailable: false,
                                     sectionAction: {
+                        showingAlert = true 
                     })
                 }
             }
+            
+            if showingAlert {
+                CustomAlert(isShowing: $showingAlert,
+                            icon: HomeScreenIcons.alertIcon,
+                            title: localized(MainMenuStrings.logoutAlertTitle),
+                            leftButtonText: localized(MainMenuStrings.logoutSectionCancel),
+                            rightButtonText: localized(MainMenuStrings.logoutSectionOk),
+                            description: localized(MainMenuStrings.logoutAlertDescription),
+                            leftButtonAction: {},
+                            rightButtonAction: { beginLogoutAnimation() },
+                            isSingleButton: false)
+            }
+            
+            if isLogoutLoading {
+                LoadingView(isShowing: $isLogoutLoading,
+                            title: localized(MainMenuStrings.loadingViewTitle),
+                            description: localized(MainMenuStrings.loadingViewDescription))
+            }
+            
         }
         .navigationBarBackButtonHidden()
+    }
+    
+    func beginLogoutAnimation() {
+        showingAlert = false
+        isLogoutLoading = true
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            logout()
+        }
+    }
+    
+    func logout() {
+        presentationMode.wrappedValue.dismiss()
     }
 }
