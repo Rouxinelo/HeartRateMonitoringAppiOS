0f0f3645c45c0b1a09b3a011dd43846663972e97
João Rouxinol <m54076@alunos.uevora.pt>
2024-11-17 14:59:19 +0000

    add view model to the screen

--- HeartRateMonitoringApp/Modules/Teacher/TeacherSessionScreen/TeacherSessionScreen.swift
+++ HeartRateMonitoringApp/Modules/Teacher/TeacherSessionScreen/TeacherSessionScreen.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct TeacherSessionScreen: View {
+    @StateObject var viewModel = TeacherSessionViewModel()
+
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
